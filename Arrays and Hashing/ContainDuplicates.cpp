69fdb02c3a55f94d544a8a9d26cbb2542c2a87fd
RodryAlexander <rodrygoleu7@gmail.com>
2025-09-01 21:21:28 -0500

    Uploading .gitignore

--- Arrays and Hashing/ContainDuplicates.cpp
+++ Arrays and Hashing/ContainDuplicates.cpp
@@ -1,18 +1,7 @@
-#include <vector>
 #include <iostream>
-#include <set>
-#include <unordered_set>
-using namespace std;
 
-
-class Solution {
-public:
-   bool containsDuplicate(vector<int>& nums) {
-      unordered_set<int> aux;
-      for(int i = 0; i < nums.size(); i++){
-         if(aux.find(nums[i])!=aux.end())  return true;
-         aux.insert(nums[i]);
-      }
-      return false;
-   }
-};
+int main()
+{
+   std::cout << "Hello World!" << std::endl;
+   return 0;
+}
