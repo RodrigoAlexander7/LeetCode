ee61d91add954f887ed5241aa5c2ceacc911c0a9
RodryAlexander <rodrygoleu7@gmail.com>
2025-09-02 22:07:18 -0500

    228 Summary Ranges - Python

--- Arrays and Hashing/ContainDuplicates.py
+++ Arrays and Hashing/ContainDuplicates.py
@@ -2,9 +2,10 @@ from typing import List
 
 
 class Solution:
-   def containsDuplicate(self, nums: List[int]) -> bool:
-      aux = set()
-      for num in nums:
-         if num in aux: return True
-         aux.add(num)
-      return False
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        aux = set()
+        for num in nums:
+            if num in aux:
+                return True
+            aux.add(num)
+        return False
