8b7ba54dfc083629c8ce81aea765b79fb9080153
RodryAlexander <rodrygoleu7@gmail.com>
2025-09-11 06:06:04 -0500

    238 Product of Array exept self python solution

--- Python/Arrays and Hashing/ProductArrayExceptSelf.py
+++ Python/Arrays and Hashing/ProductArrayExceptSelf.py
@@ -4,28 +4,27 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefixs = []
-        sufix = []
+        res = []
 
+        prefix = []
         # Calculate all the prefix
+        prefix_aux = 1
         for i in range(len(nums)):
-            if i == 0:
-                prefixs.append(1)
-                continue
-            aux = 1
-            for j in range(i):
-                aux = aux * nums[j]
-            prefixs.append(aux)
+            prefix.append(prefix_aux)
+            prefix_aux *= nums[i]
 
+        sufix = (
+            [0] * len(nums)
+        )  # initialize an array with 0 -> important cause we need to fill the array since the last element
         # Calculate all the sufix
-        for i in range(len(nums)):
-            if i == len(nums) - 1:  # last element
-                sufix.append(1)
-                continue
-            aux = 1
-            j = len(nums)
-            while j > 0:
-                aux = aux * nums[j]
-                j = j - 1
+        sufix_aux = 1
+        for i in range(
+            len(nums) - 1, -1, -1
+        ):  # the las two -1 means -> repeat until the -1th element and decrece in 1 each step
+            sufix[i] = sufix_aux
+            sufix_aux *= nums[i]
 
-        return nums
+        for i in range(len(nums)):
+            res.append(prefix[i] * sufix[i])
+
+        return res
